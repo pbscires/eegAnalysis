871f16ccc8d75c7d0cb75d60176f65ad5f9e936a
rsburugula <burugula@gmail.com>
2017-12-24 14:06:24 -0500

    A working version, to be improved shortly

--- ExtractAndTransformFeatures/extractFeature_LineLength.py
+++ ExtractAndTransformFeatures/extractFeature_LineLength.py
@@ -12,32 +12,22 @@ import sys
 import pyedflib
 from multiprocessing import Pool
 import matplotlib.pyplot as plt
+from util.ElapsedTime import ElapsedTime
 
 
 
 
-epochLength = 1000 # In milliseconds
-slidingWindowLength = 10 # In number of epochs
+epochLength = 10000 # In milliseconds
+slidingWindowLength = 2000 # In milliseconds
 
-def lineLenForTimestamp(rowIndex):
-    global numSamplesPerEpoch
-    global allChannelsDF
-    global rowDiffs
-
-    if (rowIndex < numSamplesPerEpoch):
-        return
-    else:
-        row = allChannelsDF.iloc[rowIndex-1] - allChannelsDF.iloc[rowIndex]
-        for j in range(2, numSamplesPerEpoch):
-            row = row + (allChannelsDF.iloc[rowIndex-j] - 
-                         allChannelsDF.iloc[rowIndex-j+1])
-        return row
-
+# Sliding window length should be < epocLength
+if (slidingWindowLength > epochLength):
+    print ("Invalid values for sliding window length and/or epoch length")
+    exit(-1)
 
 def calculateLineLength(filename):
     global numSamplesPerEpoch
     global allChannelsDF
-    global rowDiffsDf
 
     if (re.search('\.edf', filename) != None):
         f = pyedflib.EdfReader(sys.argv[1])
@@ -50,15 +40,14 @@ def calculateLineLength(filename):
         numSamples = f.getNSamples()[0]
         # sampleFrequency = 256
         sampleFrequency = f.getSampleFrequency(0)
-        numSamplesPerEpoch = int(sampleFrequency * 1000 / epochLength)
+        numSamplesPerEpoch = int(sampleFrequency * epochLength / 1000)
         sigbufs = np.zeros((numChannels, numSamples))
         for i in np.arange(numChannels):
             sigbufs[i, :] = f.readSignal(i)
-        # sifbufs above is a 23 x 921600 matrix
+        # sigbufs above is a 23 x 921600 matrix
         # transpose it so that it becomes 921600 x 23 matrix
         sigbufs = sigbufs.transpose()
         allChannelsDF = pd.DataFrame(data = sigbufs, columns = signal_labels)
-        rowDiffsDf = pd.DataFrame(columns = signal_labels)
         print ("Shape of allChannelsDf = ", allChannelsDF.shape)
         print (allChannelsDF.head(10))
         
@@ -68,13 +57,32 @@ def calculateLineLength(filename):
         lastRowInd = numSamples - 2
         endOfHeadInd = 0
         begOfTailInd = lastRowInd-numSamplesPerEpoch+1
-        llMat = np.delete(sigDiffs, range(begOfTailInd, lastRowInd+1), 0)
+
+        startingRowsArr = np.arange(0, numSamples, slidingWindowLength)
+        print (startingRowsArr.shape)
+#         print(startingRowsArr)
+        j = len(startingRowsArr)
+        j -= 1
+        print ("j=", j, "startingRowsArr[", j, "] = ", startingRowsArr[j])
+        lastEpochIndex = numSamples - (epochLength*sampleFrequency / 1000)
+        print ("lastEpochIndex = ", lastEpochIndex)
+        while (startingRowsArr[j] > lastEpochIndex):
+            j -= 1
+            print ("j=", j, "startingRowsArr[", j, "] = ", startingRowsArr[j])
+        startingRowsArr = np.delete(startingRowsArr, range(j,len(startingRowsArr)))
+        print ("j=", j)
+        print (startingRowsArr.shape)
+        
+        llMat = np.delete(sigDiffs, list(range(begOfTailInd, lastRowInd+1)), 0)
+        timer2 = ElapsedTime()
         for j in range(1, numSamplesPerEpoch):
+            timer2.reset()
             endOfHeadInd += 1
             begOfTailInd += 1
             rowsToDel = list(range(endOfHeadInd)) + list(range(begOfTailInd, lastRowInd+1))
 #             print ("Removing the rows", rowsToDel)
             llMat = llMat + np.delete(sigDiffs, rowsToDel, 0)
+            print("j=", j, ", timer2 elapsed time=", timer2.timeDiff())
         llDf = pd.DataFrame(data = llMat, columns = signal_labels)
                 
         print ("Printing Line Length Data frame for file", filename)
@@ -90,6 +98,8 @@ def calculateLineLength(filename):
 #p.map(calculateLineLength, [filesList[0]])
 #print (filesList[0])
 #calculateLineLength(filesList[0])
-    
+timer1 = ElapsedTime()
+timer1.reset()
 print (sys.argv[1])
 calculateLineLength(sys.argv[1])
+print(timer1.timeDiff())
