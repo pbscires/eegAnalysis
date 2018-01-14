bd0ab4b70d29fdfce8f0d611015f887f98159b03
Pranav Burugula <pranav.burugula@gmail.com>
2018-01-14 16:29:09 -0500

    Added SVM

--- TrainAndTest/SVMClassifier.py
+++ TrainAndTest/SVMClassifier.py
@@ -1,36 +1,34 @@
 '''
-Created on Dec 26, 2017
+Created on Jan 14, 2018
 
 @author: pb8xe
 '''
-
+from sklearn.svm import SVC
 import numpy as np
+from sklearn.preprocessing import StandardScaler
 from sklearn.cross_validation import train_test_split
-from sklearn.preprocessing.data import StandardScaler
-from sklearn.neighbors.classification import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
-
-class KNNClassifier():
+class SVMClassifier(object):
     '''
     classdocs
     '''
 
 
-    def __init__(self, csv_path, k):
+    def __init__(self, csv_path):
         '''
         Constructor
         '''
         self.csv_path = csv_path
-        self.classifier = KNeighborsClassifier(n_neighbors=k, p=2, metric='minkowski')
+        self.classifier = SVC(kernel='rbf', random_state=0, gamma=.1, C=10.0)
     
     def create_arrays(self):
         arr = np.genfromtxt(self.csv_path, delimiter=',')
         print(arr.shape)
         self.X = np.delete(arr, [arr.shape[1]-1], axis=1)
-        self.X = np.delete(self.X, [0], axis=0)
+        self.X = np.delete(self.X, 0, axis=0)
         print(self.X.shape)
         self.y = np.delete(arr, list(range(arr.shape[1]-1)), axis=1)
-        self.y = np.delete(self.y, [0], axis=0)
+        self.y = np.delete(self.y, 0, axis=0)
         print(self.y.shape)
     
     def preprocess(self):
@@ -45,4 +43,4 @@ class KNNClassifier():
     
     def test(self):
         y_pred = self.classifier.predict(self.X_test_std)
-        print('Accuracy: %.2f' % accuracy_score(self.y_test, y_pred))
+        print("Accuracy: %.2f" % accuracy_score(self.y_test, y_pred))
