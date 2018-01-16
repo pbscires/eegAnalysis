67e821bb2b85320aca636bbc50db72d3773fc2ec
Pranav Burugula <pranav.burugula@gmail.com>
2018-01-15 21:13:01 -0500

    Final v0.1

--- mainScripts/useClassifier.py
+++ mainScripts/useClassifier.py
@@ -11,17 +11,15 @@ from TrainAndTest.SVMClassifier import SVMClassifier
 if __name__ == '__main__':
     print(sys.argv[1])
     print(sys.argv[2])
-#     knn_classifier = KNNClassifier(sys.argv[1], sys.argv[2], k=5)
-#     knn_classifier.train()
-#     knn_classifier.test()
-# 
-#     svm_classifier = SVMClassifier(sys.argv[1], sys.argv[2])
-#     svm_classifier.train()
-#     svm_classifier.test()
-#     
+    
+    knn_classifier = KNNClassifier(sys.argv[1], sys.argv[2], k=5)
+    knn_classifier.train()
+    knn_classifier.test()
+ 
+    svm_classifier = SVMClassifier(sys.argv[1], sys.argv[2])
+    svm_classifier.train()
+    svm_classifier.test()
+     
     dnn_classifier = DNNClassifier(sys.argv[1], sys.argv[2])
     dnn_classifier.train()
     dnn_classifier.test()
-
-#     svm_classifier = SVMClassifier(sys.argv[1], sys.argv[2])
-#     svm_classifier.validation()
